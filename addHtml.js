c60498bdb9011a3da0c622f0bd2dc0764544cd75
NAVDEEP BHATT <NAVDEEP9557@gmail.com>
2025-03-16 13:38:26 +0530

    Changes saved

--- addHtml.js
+++ addHtml.js
@@ -2,8 +2,27 @@ const express = require("express");
 const app = express();
 
 app.get("", (req, res) => {
-  console.log("<h1>This Is Heading 1 </h1>")
-  res.send("Welcome "+req.query.name);
+  res.send(`<h1>This Is Heading 1 </h1>  <a href ="/about">About Us </a> <br><br>
+    <a href ="/help"> Help page </a>  `);
+});
+
+app.get("/about", (req, res) => {
+  res.send(`
+    <a href ="/">Home Page </a> <br><br><br><br>
+    <input type = "text" placeholder = "username"/>
+    <button> Click Me </button>
+    `);
+});
+
+app.get("/help", (req, res) => {
+  res.send(
+   `
+    <a href ="/">Home Page </a> <br><br><br><br>
+    {
+    name: "navdeep bhatt",
+    age: 20,
+    contact: "844536367x",
+  }`);
 });
 
 app.listen(5000);
